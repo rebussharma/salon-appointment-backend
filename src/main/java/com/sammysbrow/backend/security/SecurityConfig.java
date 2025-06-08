da27339fb776c1dc1d742cfb6a505b4d0c619c41
Ribash Sharma <ribashsharma@Ribashs-MBP.attlocal.net>
2025-06-08 17:40:12 -0500

    added imports

--- src/main/java/com/sammysbrow/backend/security/SecurityConfig.java
+++ src/main/java/com/sammysbrow/backend/security/SecurityConfig.java
@@ -1,8 +1,19 @@
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
-    @Value(security.api.bearer.token)
+    @Value("${security.api.token}")
     private String apiToken;
 
     @Override
